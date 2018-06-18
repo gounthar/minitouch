3c3ba0a46642f46f37997d8cbad36ca68dce36dc
Bruno Verachten <116569+gounthar@users.noreply.github.com>
2018-06-18 15:23:59 +0200

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Download and install the NDK
-wget http://dl.google.com/android/ndk/android-ndk-${ANDROID_NDK_VERSION}-linux-x86_64.bin -O travis/android-ndk-${ANDROID_NDK_VERSION}-linux-x86_64.bin
-chmod +x travis/android-ndk-${ANDROID_NDK_VERSION}-linux-x86_64.bin
-./travis/android-ndk-${ANDROID_NDK_VERSION}-linux-x86_64.bin -otravis/ -y > /dev/null
+wget http://dl.google.com/android/ndk/android-ndk-${ANDROID_NDK_VERSION}-linux-x86_64.zip -O travis/android-ndk-${ANDROID_NDK_VERSION}-linux-x86_64.zip
+unzip travis/android-ndk-${ANDROID_NDK_VERSION}-linux-x86_64.zip
 
 # Dump the environment variables
 echo "ANDROID_NDK_HOME set to $ANDROID_NDK_HOME"
